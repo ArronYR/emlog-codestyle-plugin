ebff2b3c189c509b9916cadf2a10535226120fcf
ArronYR <yangyun4814@gmail.com>
2016-01-28 11:22:27 +0800

    完成在后台对插件的设置

--- codestyle_setting.php
+++ codestyle_setting.php
@@ -5,9 +5,8 @@
 
 !defined('EMLOG_ROOT') && exit('access deined!');
 
-require_once 'codestyle_config.php';
-
 function plugin_setting(){
+    require_once 'codestyle_config.php';
     $style = $_POST["style"]=="" ? "default": $_POST["style"];
     $numlines = $_POST["numlines"]=="" ? "no": $_POST["numlines"];
     $on = $_POST["on"]=="" ? "no" : $_POST["on"];
@@ -20,6 +19,10 @@ $config = array(
     echo $newConfig;
     @file_put_contents(EMLOG_ROOT.'/content/plugins/codestyle/codestyle_config.php', $newConfig);
 }
+
+function plugin_setting_view(){
+    require_once 'codestyle_config.php';
+    $styles = array('default', 'desert', 'doxy', 'obsidian', 'sunburst');
 ?>
 
 <link rel="stylesheet" type="text/css" href="<?php echo BLOG_URL;?>content/plugins/codestyle/css/codestyle.css"/>
@@ -35,17 +38,43 @@ $config = array(
             <ul>
                 <li>
                     <h4>是否启用该插件</h4>
-                    <div class="radio"><input type="radio" name="on" value="yes"<?php if($config["on"] == 'yes'){echo ' checked="checked"';}?> /><label>是</label></div>
-                    <div class="radio"><input type="radio" name="on" value="no"<?php if($config["on"]=='no'){echo ' checked="checked"';}?> /><label>否</label></div>
+                    <div class="radio">
+                        <input type="radio" name="on" value="yes" <?php if($config["on"] == 'yes'){echo ' checked="checked"';}?> />
+                        <label>是</label>
+                    </div>
+                    <div class="radio">
+                        <input type="radio" name="on" value="no" <?php if($config["on"]=='no'){echo ' checked="checked"';}?> />
+                        <label>否</label>
+                    </div>
                     <p>如果启用该插件，页面代码样式将被替换，如不需要选择不启用</p>
                 </li>
                 <li>
                     <h4>开启代码行号</h4>
-                    <div class="radio"><input type="radio" name="numlines" value="yes"<?php if($config["numlines"] == 'yes'){echo ' checked="checked"';}?> /><label>是</label></div>
-                    <div class="radio"><input type="radio" name="numlines" value="no"<?php if($config["numlines"]=='no'){echo ' checked="checked"';}?> /><label>否</label></div>
+                    <div class="radio">
+                        <input type="radio" name="numlines" value="yes" <?php if($config["numlines"] == 'yes'){echo ' checked="checked"';}?> />
+                        <label>是</label>
+                    </div>
+                    <div class="radio">
+                        <input type="radio" name="numlines" value="no" <?php if($config["numlines"]=='no'){echo ' checked="checked"';}?> />
+                        <label>否</label>
+                    </div>
                     <p>如果需要在代码前显示行号，请开启该选项，如不需要选择不开启</p>
                 </li>
-                <div class="sl">
+                <li>
+                    <h4>选择样式</h4>
+                    <div class="select">
+                        <select name="style">
+                            <?php foreach ($styles as $i => $style):?>
+                                <option value="<?php echo $style;?>" <?php if($config["style"]==$style){echo ' selected="selected"';}?>><?php echo $style;?></option>
+                            <?php endforeach;?>
+                        </select>
+                    </div>
+                    <p>共5种样式:default, desert, doxy, obsidian, sunburst。默认样式default。</p>
+                </li>
+                <div class="cs-image">
+                    <img src="" id="preview-image">
+                </div>
+                <div class="cs-tips">
                     注意事项：<br />
                     1、模板必须含有挂载点(没有请自行加上)：&lt;?php doAction('index_footer');?&gt;<br />
                     &nbsp;&nbsp;&nbsp;&nbsp;修改正在使用的主题下的 footer.php ,在body标签结束之前加上。<br/>
@@ -58,3 +87,16 @@ $config = array(
         </form>
     </div>
 </div>
+<script type="text/javascript">
+jQuery(document).ready(function($) {
+    var imageName = $('select').val();
+    $('#preview-image').attr('src', '/content/plugins/codestyle/images/'+imageName+".png");
+    $('select').on('change', function(event) {
+        event.preventDefault();
+        imageName = $(this).val();
+        $('#preview-image').attr('src', '/content/plugins/codestyle/images/'+imageName+".png");
+    });
+});
+</script>
+
+<?php }?>
